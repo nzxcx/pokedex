2c91d1c721f65af23b88a5c8aadeba17ab4ecf08
nzxcx <hugo.laclau@gmail.com>
2025-03-16 19:57:23 +0100

    feat(client): move to tanstack router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,8 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import { ThemeProvider } from "@/components/theme/theme-provider"
-import { ThemeToggle } from "@/components/theme/theme-toggle"
-import { PokemonList } from "./components/pokemon-list"
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      gcTime: 1000 * 60 * 60, // 1 hour
-      retry: 1,
-    },
-  },
-})
+import { RouterProvider } from '@tanstack/react-router'
+import { router } from './router'
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
-        <div className="min-h-screen bg-gradient-to-b from-background to-background/80">
-          <div className="fixed top-4 right-4 z-50">
-            <ThemeToggle />
-          </div>
-          <PokemonList />
-        </div>
-      </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
